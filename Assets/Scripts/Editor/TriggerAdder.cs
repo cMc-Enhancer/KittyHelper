c25819d10955052f4be935787527d9c67dbc59de
Enhancer <xjhxyshz@126.com>
2023-02-15 18:27:19 +0800

    Get animator controller automatically, ensure trigger exists before adding it

--- Assets/Scripts/Editor/TriggerAdder.cs
+++ Assets/Scripts/Editor/TriggerAdder.cs
@@ -1,69 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 using UnityEditor;
 using UnityEditor.Animations;
 using UnityEngine;
 
 public class TriggerAdder : MonoBehaviour
 {
-    private static AnimatorController s_Controller;
     private static readonly string[] s_Triggers = {"aTriggerParameter", "bTriggerParameter"};
     private static int s_Index;
-
-    [MenuItem("Jobs/SetAnimatorController %t")]
-    public static void SetAnimatorController()
-    {
-        AnimatorController[] controllers = Selection.GetFiltered<AnimatorController>(SelectionMode.Assets);
-        if (controllers.Length == 1)
-        {
-            s_Controller = controllers[0];
-            s_Index = 0;
-            Debug.LogFormat("[TriggerAdder] Animator controller: {0}", s_Controller.name);
-        }
-        else
-        {
-            Debug.LogError("[TriggerAdder] Only 1 animator controller should be selected");
-        }
-    }
 
     [MenuItem("Jobs/AddParameters %y")]
     public static void AddParameters()
     {
-        if (s_Controller == null)
+        AnimatorController controller = GetAnimatorController();
+        if (controller == null)
         {
             Debug.LogError("[TriggerAdder] Animator controller not set");
             return;
         }
-        
+
         foreach (string triggerName in s_Triggers)
         {
-            s_Controller.AddParameter(triggerName, AnimatorControllerParameterType.Trigger);
+            controller.AddParameter(triggerName, AnimatorControllerParameterType.Trigger);
         }
     }
 
     [MenuItem("Jobs/AddTrigger %g")]
     public static void AddTriggerToTransition()
     {
-        if (s_Controller == null)
-        {
-            Debug.LogError("[TriggerAdder] Animator controller not set");
-            return;
-        }
-
         if (s_Index >= s_Triggers.Length)
         {
-            Debug.LogError("[TriggerAdder] All triggers added, do nothing");
+            Debug.LogErrorFormat("[TriggerAdder] All triggers added, do nothing");
             return;
         }
 
-        AnimatorStateTransition[] transitions = Selection.GetFiltered<AnimatorStateTransition>(SelectionMode.Editable);
+        string triggerToAdd = s_Triggers[s_Index];
+        if (!IsTriggerExists(triggerToAdd))
+        {
+            Debug.LogErrorFormat("[TriggerAdder] Trigger {0} does not exist", triggerToAdd);
+            return;
+        }
+
+        AnimatorStateTransition[] transitions =
+            Selection.GetFiltered<AnimatorStateTransition>(SelectionMode.Editable);
         if (transitions.Length == 0)
         {
-            Debug.LogError("[TriggerAdder] No transition is selected");
+            Debug.LogErrorFormat("[TriggerAdder] No transition is selected");
             return;
         }
 
         foreach (AnimatorStateTransition transition in transitions)
         {
-            transition.AddCondition(AnimatorConditionMode.If, 0, s_Triggers[s_Index]);
+            transition.AddCondition(AnimatorConditionMode.If, 0, triggerToAdd);
         }
 
         s_Index++;
@@ -71,12 +60,57 @@ public class TriggerAdder : MonoBehaviour
         Debug.LogFormat("[TriggerAdder] Trigger added");
     }
 
+    private static AnimatorController GetAnimatorController()
+    {
+        EditorWindow window = EditorWindow.focusedWindow;
+        Type windowType = window.GetType();
+
+        AnimatorController controller = null;
+        if (windowType.Name != "AnimatorControllerTool")
+        {
+            Debug.LogErrorFormat("[TriggerAdder] Animator window is not current active window");
+        }
+
+        PropertyInfo controllerProperty = windowType.GetProperty("animatorController",
+            BindingFlags.NonPublic | BindingFlags.Public |
+            BindingFlags.Instance);
+        if (controllerProperty != null)
+        {
+            controller = controllerProperty.GetValue(window, null) as AnimatorController;
+        }
+        else
+        {
+            Debug.LogErrorFormat("[TriggerAdder] Cannot find animatorController");
+        }
+
+        return controller;
+    }
+
+    private static bool IsTriggerExists(string name)
+    {
+        AnimatorController controller = GetAnimatorController();
+        if (controller == null)
+        {
+            Debug.LogError("[TriggerAdder] Animator controller not set");
+            return false;
+        }
+
+        HashSet<string> triggers = new HashSet<string>(controller.parameters.Length);
+        foreach (AnimatorControllerParameter parameter in controller.parameters)
+        {
+            if (parameter.type == AnimatorControllerParameterType.Trigger)
+            {
+                triggers.Add(parameter.name);
+            }
+        }
+
+        return triggers.Contains(name);
+    }
+
     [MenuItem("Jobs/Forget %j")]
     public static void Forget()
     {
-        s_Controller = null;
         s_Index = 0;
         Debug.LogFormat("[TriggerAdder] Forgot");
     }
-    
 }
